85c41f2aae0b2a26be8010a4d32b5efa975f1d27
Danil <de.myskov@gmail.com>
2021-02-24 22:58:28 +0700

    Corrected mistakes

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void check_circle(char* search, int m_string){
+void check_circle(char* search, int m_string)
+{
     char check_string[] = {'c', 'i', 'r', 'c', 'l', 'e', '('};
     int is_true = 0;
     int i, j, n, k;
     //char x, y, r;
+    
     //правильность написания + ргеистр
     for (i = 0; i < 7; ++i) {
         if (tolower(search[i]) != check_string[i]) {
@@ -18,49 +20,64 @@ void check_circle(char* search, int m_string){
         if (is_true != 0) {
             printf("\nError: expected 'circle': ");
         }
+    
     //переход на первое значение в скобках, то есть на занчение 'x'
-    for (i =0; i < m_string; i++)
+    for (i = 0; i < m_string; i++)
     {
-        if (search[i] == '(' ) {
+        if (search[i] == '(')
+        {
             for (j = i; search[j+1] != ' '; j++)
             {
-                if ((search[j+1] <= '9') && (search[j+1] >= '1')) {
+                if ((search[j+1] <= '9') && (search[j+1] >= '0')) {
                 }
                 else{
-                    for (n =0; n != (j+1); n++) {
+                    for (n = 0; n != (j+1); n++)
+                    {
                         printf(" ");
                     }
                     printf("\nError: expected x '<double>'");
-                    exit(0);
-                    
                 }
             }
         }
     }
+    
     //переход на значение 'y'
-    for (i =0; i < m_string; i++) {
-        if (search[i] == ' ') {
-            for (j = i; search[j+1] != ' '; j++) {
-                if ((search[j+1]<='9') && (search[j+1]>='1')) {
-                }
-                else{
-                    for (n =0; n != (j+1); n++) {
-                        printf(" ");
+    for (i =0; i < m_string; i++)
+    {
+        if (search[i] == ' ')
+        {
+            for (k = 1; k < m_string; k++)
+            {
+                if (search[i] == ',')
+                {
+                    for (j = i; search[j+1] != ' '; j++)
+                    {
+                        if ((search[j+1]<='9') && (search[j+1]>='0')) {
+                        }
+                        else{
+                            for (n = 0; n != (j+1); n++) {
+                                printf(" ");
+                            }
+                            printf("\nError: expected y '<double>'");
+                        }
                     }
-                    printf("\nError: expected y '<double>'");
-                    exit(0);
                 }
             }
         }
     }
+   
     //проверка значения 'r'
-    for (i = 0; i < m_string; i++) {
-        if ((search[i] == ',') && (search[i+1] == ' ')) {
-            for (j = (i+1); search[j+1] != ')'; j++) {
-                if ((search[j+1] <= '9') && (search[j+1] >= '1')) {
+    for (i = 0; i < m_string; i++)
+    {
+        if ((search[i] == ',') && (search[i+1] == ' '))
+        {
+            for (j = (i+1); search[j+1] != ')'; j++)
+            {
+                if ((search[j+1] <= '9') && (search[j+1] >= '0')) {
                 }
                 else{
-                    for (n = 0; n != (j+1); n++) {
+                    for (n = 0; n != (j+1); n++)
+                    {
                         printf(" ");
                     }
                     printf("\nError: expected r '<double>'");
@@ -89,9 +106,7 @@ int main()
     printf("Enter your information as shown on the example: \nObject '(' Point ',' Number ')'\nPoint = Number Number\nNumber = (* Floating-point number *)\nEnter please: ");
     scanf("%[^\n]s",search);
     check_circle(search, m_string);
-    
-    
-
+    output(search, m_string);
 }
 
 

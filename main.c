d3681df7d72350229cd2f5d0fd74e2574d442687
danyawhatsup <78411198+danyawhatsup@users.noreply.github.com>
2021-03-21 15:06:27 +0700

    Update main.c
    reworked the check y function and fixed compilation errors

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void check_circle(char* search, int m_string)
 {
     char check_string[] = {'c', 'i', 'r', 'c', 'l', 'e', '('};
     int is_true = 0;
-    int i, j, n;
+    int i, j, n, g;
     //char x, y, r;
     
     //правильность написания + ргеистр
@@ -21,6 +21,24 @@ void check_circle(char* search, int m_string)
             printf("\nError: expected 'circle': ");
             exit(0);
         }
+    //проверка на символы после ')'
+    for (i = 0; i < m_string; i++)
+        {
+            if (search[i] == ')')
+            {
+                if (search[i+1] == '\0'){
+                }
+                else{
+                    for (j = 0; j != (i+1); j++)
+                    {
+                        printf(" ");
+                    }
+                    printf("^");
+                    printf("\nError: unexpected token");
+                    exit(0);
+                }
+            }
+        }
     
     //переход на первое значение в скобках, то есть на занчение 'x'
     for (i = 0; i < m_string; i++)
@@ -29,7 +47,7 @@ void check_circle(char* search, int m_string)
         {
             for (j = i; search[j+1] != ' '; j++)
             {
-                if ((search[j+1] <= '9') && (search[j+1] >= '0')) {
+                if (((search[j+1]<='9') && (search[j+1]>='1')) || (search[j+1] =='.')) {
                 }
                 else{
                     for (n = 0; n != (j+1); n++)
@@ -45,33 +63,40 @@ void check_circle(char* search, int m_string)
     
     //переход на значение 'y'
     for (i = 0; i < m_string; i++)
-    {
-        if (search[i] == " ")
         {
-            for (j = i; search[j+1] != ','; j++)
+            if (search[i] == ' ')
             {
-                if ((search[j+1] <= '9') && (search[j+1] >= '0')) {
-                }
-                else{
-                    for (n = 0; n != (j+1); n++)
+                for (g = i; g < m_string; g++)
+                {
+                    if (search[g] == ',')
                     {
-                        printf(" ");
+                        for (j = i; search[j + 1] != ','; j++)
+                        {
+                            if (((search[j+1]<='9') && (search[j+1]>='1')) || (search[j+1] =='.')){
+                            }
+                            else
+                            {
+                                for (n = 0; n != (j+1); n++){
+                                    printf(" ");
+                                }
+                                printf("\nError: expected y '<double>'");
+                        
+                                exit(0);
+                            }
+                        }
                     }
-                    printf("\nError: expected y '<double>'");
-                    exit(0);
                 }
             }
         }
-    }
    
     //проверка значения 'r'
     for (i = 0; i < m_string; i++)
     {
-        if ((search[i] == ',') && (search[i+1] == " "))
+        if ((search[i] == ',') && (search[i+1] == ' '))
         {
             for (j = (i+1); search[j+1] != ')'; j++)
             {
-                if ((search[j+1] <= '9') && (search[j+1] >= '0')) {
+                if (((search[j+1]<='9') && (search[j+1]>='1')) || (search[j+1] =='.')) {
                 }
                 else{
                     for (n = 0; n != (j+1); n++)
@@ -132,4 +157,3 @@ int main()
     output(search, m_string);
     p_and_a(search, m_string);
 }
-
